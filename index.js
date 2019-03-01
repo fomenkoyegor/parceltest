6fc556099e590a2bc77ea130274c544cb4e9f3c1
fomenkoyegor <fomenkoyegor@gmail.com>
2019-03-01 03:57:42 +0200

    some styles

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 import jquery from "jquery";
 import 'bootstrap';
+
 export default (window.$ = window.jQuery = jquery);
 
 
+const menu = $('.menu');
+const burger = $('.burger');
+const section = $('section');
+const upBtn = $('.up');
+
+upBtn.on("click",e=>{
+    $('html, body').animate({
+                    scrollTop: 0
+                }, 600);
+});
+burger.on("click", e => {
+    burger.toggleClass('show');
+    menu.toggleClass('show');
+});
